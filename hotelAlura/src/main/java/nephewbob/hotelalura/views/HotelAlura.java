59db87d7b441ec9c5c0b0e33135a05752a6df4a6
Kraken <mario.ballestserosp@gmail.com>
2023-11-08 15:08:52 -0600

    Imported the following classes by Alura: - Busqueda - Exito - Login - MenuPrincipal - MenuUsuario - RegistroHuesped - ReservasView

---
+++ hotelAlura/src/main/java/nephewbob/hotelalura/views/HotelAlura.java
@@ -0,0 +1,12 @@
+package nephewbob.hotelalura.views;
+
+/**
+ *
+ * @author Mario Alberto Ballesteros Plata <MBallesterosp at nephewbob>
+ */
+public class HotelAlura {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
